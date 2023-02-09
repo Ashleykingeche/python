4bab8904810053b8b89f75c28c7fa729a3111488
Ashleykingeche <waruguruashley@gmail.com>
2023-02-09 11:29:17 +0300

    lesson 1

---
+++ condition.py
@@ -0,0 +1,10 @@
+num=eval(input("Enter a number:"))
+if num<0:
+    print("Wrong input")
+else:
+    if num>50:
+        x=num/2
+        print(x)
+    
+    else:
+        print("Number is less than 50")
